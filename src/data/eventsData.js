2f6b4ba7096588337de69f42941eccdf7d85c9f4
Kurasakirookia <tejaskumardamodar@gmail.com>
2025-05-15 21:38:33 +0530

    Remove node_modules from repo

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -1,48 +1,48 @@
 const eventsData = {
-    all: [
-      {
-        title: "Event 1",
-        description: "Lorem ipsum dolor sit amet.",
-        deadline: "7/3/25",
-        image: "/assets/event1.png",
-      },
-      {
-        title: "Event 2",
-        description: "Consectetur adipiscing elit.",
-        deadline: "10/4/25",
-        image: "/assets/event2.png",
-      }
-    ],
-    past: [
-      {
-        title: "Past Event 1",
-        description: "This event happened in the past.",
-        deadline: "1/1/24",
-        image: "/assets/past1.png",
-      }
-    ],
-    ongoing: [
-      {
-        title: "Ongoing Event 1",
-        description: "Currently happening!",
-        deadline: "Ongoing",
-        image: "/assets/ongoing1.png",
+  all: [
+    {
+      title: "Event 1",
+      description: "Lorem ipsum dolor sit amet.",
+      deadline: "7/3/25",
+      image: "/assets/event1.png",
+    },
+    {
+      title: "Event 2",
+      description: "Consectetur adipiscing elit.",
+      deadline: "10/4/25",
+      image: "/assets/event2.png",
+    }
+  ],
+  past: [
+    {
+      title: "Past Event 1",
+      description: "This event happened in the past.",
+      deadline: "1/1/24",
+      image: "/assets/past1.png",
+    }
+  ],
+  ongoing: [
+    {
+      title: "Ongoing Event 1",
+      description: "Currently happening!",
+      deadline: "Ongoing",
+      image: "/assets/ongoing1.png",
+    },
+    {
+      title: "Ongoing Event 2",
+      description: "Currently happening!!!!!!!!!!!",
+      deadline: "Ongoing",
+      image: "/assets/ongoing1.png",
+    }
+  ],
+  upcoming: [
+    {
+      title: "Upcoming Event 1",
+      description: "Coming soon!",
+      deadline: "15/6/25",
+      image: "/assets/upcoming1.png",
+    }
+  ]
+};
 
-        title: "Ongoing Event 2",
-        description: "Currently happening!!!!!!!!!!!",
-        deadline: "Ongoing",
-        image: "/assets/ongoing1.png",
-      }
-    ],
-    upcoming: [
-      {
-        title: "Upcoming Event 1",
-        description: "Coming soon!",
-        deadline: "15/6/25",
-        image: "/assets/upcoming1.png",
-      }
-    ]
-  };
-    
-  export default eventsData;
-  
+export default eventsData;
