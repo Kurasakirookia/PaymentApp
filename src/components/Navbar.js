23f74ee331f3d937e92786b4eca551fc5e6d477f
Kurasakirookia <tejaskumardamodar@gmail.com>
2025-05-15 14:06:25 +0530

    added predeploy and deploy in scripts for deploying in github

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "../css/Navbar.css";
 import logo from "../assets/samyog_logo_letters.png"; // Adjust path if needed
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav>
       <ul className="nav_list">
-        <li className="nav_item">Home</li>
-        <li className="nav_item">Clubs</li>
-        <li className="nav_item">Teams</li>
-        <li className="nav_item">Events</li>
+        <Link to="/" className="nav_item">Home</Link>
+        <Link to="events" className="nav_item">Clubs</Link>
+        <Link className="nav_item">Teams</Link>
+        <Link className="nav_item">Events</Link>
       </ul>
       <img src={logo} alt="logo" className="logo" />
       <p className="gallery">Gallery</p>
